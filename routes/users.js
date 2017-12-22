71d1b5c63236508afec40ce1e678bbd513251426
Kevin Cottington <kevin.cottington@honeywell.com>
2017-12-22 10:18:42 -0500

    Added one route

--- routes/users.js
+++ routes/users.js
@@ -1,15 +1,22 @@
 var express = require('express');
 var router = express.Router();
 
+var users = [{
+	id: 1,
+	username: "samsepi0l"
+}, {
+	id: 2,
+	username: "D0loresH4ze"
+}]
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.json([{
-  	id: 1,
-  	username: "samsepi0l"
-  }, {
-  	id: 2,
-  	username: "D0loresH4ze"
-  }]);
+  res.json(users);
 });
+
+router.post('/', function(req, res, next) {
+	console.log(req.body)
+	res.send('OK')
+})
 
 module.exports = router;
